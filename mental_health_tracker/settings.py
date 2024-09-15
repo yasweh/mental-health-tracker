c8f5d11c667a5995fe9c5c365c08b8be3ad2e885
yasweh <myayyas@gmail.com>
2024-09-15 11:18:05 +0700

    Github PWS secret

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/web/project/muhammad.yahya32/mentalhealthtracker2", "https://pbp.cs.ui.ac.id/web/project/muhammad.yahya32/mentalhealthtracker2"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/web/project/muhammad.yahya32/mentalhealthtracker", "https://pbp.cs.ui.ac.id/web/project/muhammad.yahya32/mentalhealthtracker"]
